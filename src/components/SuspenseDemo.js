a3aaad98c47ada3667f56b23113f041fa7e6d025
React Mastering <pga.adam.scott@gmail.com>
2022-04-21 23:01:55 +0900

    updated

--- src/components/SuspenseDemo.js
+++ src/components/SuspenseDemo.js
@@ -11,6 +11,33 @@ export const SuspenseDemo = () => {
   return (
     <Layout>
       <p className="mb-3 text-xl font-bold text-blue-500">Suspense Demo</p>
+      <ErrorBoundary
+        fallback={
+          <ExclamationCircleIcon className="my-5 h-10 w-10 text-pink-500" />
+        }
+      >
+        <Suspense fallback={<Spinner />}>
+          <FetchUsers />
+        </Suspense>
+      </ErrorBoundary>
+      <ErrorBoundary
+        fallback={
+          <ExclamationCircleIcon className="my-5 h-10 w-10 text-pink-500" />
+        }
+      >
+        <Suspense fallback={<Spinner />}>
+          <FetchTasks />
+        </Suspense>
+      </ErrorBoundary>
+      <ErrorBoundary
+        fallback={
+          <ExclamationCircleIcon className="my-5 h-10 w-10 text-pink-500" />
+        }
+      >
+        <Suspense fallback={<Spinner />}>
+          <FetchComments />
+        </Suspense>
+      </ErrorBoundary>
       {/* <ErrorBoundary
         fallback={
           <ExclamationCircleIcon className="my-5 h-10 w-10 text-pink-500" />
@@ -18,37 +45,10 @@ export const SuspenseDemo = () => {
       >
         <Suspense fallback={<Spinner />}>
           <FetchUsers />
-        </Suspense>
-      </ErrorBoundary>
-      <ErrorBoundary
-        fallback={
-          <ExclamationCircleIcon className="my-5 h-10 w-10 text-pink-500" />
-        }
-      >
-        <Suspense fallback={<Spinner />}>
           <FetchTasks />
-        </Suspense>
-      </ErrorBoundary>
-      <ErrorBoundary
-        fallback={
-          <ExclamationCircleIcon className="my-5 h-10 w-10 text-pink-500" />
-        }
-      >
-        <Suspense fallback={<Spinner />}>
           <FetchComments />
         </Suspense>
       </ErrorBoundary> */}
-      <ErrorBoundary
-        fallback={
-          <ExclamationCircleIcon className="my-5 h-10 w-10 text-pink-500" />
-        }
-      >
-        <Suspense fallback={<Spinner />}>
-          <FetchUsers />
-          <FetchTasks />
-          <FetchComments />
-        </Suspense>
-      </ErrorBoundary>
     </Layout>
   )
 }
